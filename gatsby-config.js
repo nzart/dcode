97ca8f3b6e281692773c8d32784a709cccaaa63f
Nick Zart <nzart@g.clemson.edu>
2020-06-09 18:13:29 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Hello',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>I'm Nick Zart</h1>
+    <p>I am a UI designer & developer.</p>
+    <p>New paragraph.</p>
+    <Link to="/page-2/">Hey!</Link>
+  </div>
+)
+
+export default IndexPage
